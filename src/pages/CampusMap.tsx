8325380266f44c939c8dfdb147996ff9a1943836
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-07-05 23:42:12 +0900

    change map size

--- src/pages/CampusMap.tsx
+++ src/pages/CampusMap.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function CampusMap() {
   return (
-    <div className="relative">
-      <img src={campusMapImage} alt="campus map" />
-      <div className="absolute top-[70%] left-[60%] bg-white">
+    <div className="relative h-screen">
+      <img src={campusMapImage} alt="campus map" className="h-full" />
+      <div className="absolute top-[70%] left-[40%] bg-white">
         <Link to="library">駒場図書館</Link>
       </div>
     </div>
